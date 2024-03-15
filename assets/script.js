37ed5d2ef4c639660bfdb31b281c2abb7f513da5
Pema Sherpa <pematamang111@gmail.com>
2024-03-15 19:26:27 -0400

    add styles

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//dependencies=========================
+const weatherDetail = document.querySelector('#weatherDetail');
+const dayFive = document.querySelector('#dayFive');
+const apiKey = 7cb9b0e18cc07a883b919195c72f4dcd;
+
+
+//function==============================
+function getApi(){
+const weatherUrl = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code},&limit=5&appid={apiKey}
+
+fetch(weatherUrl)
+   .then(function(response){
+       return response.json();
+})
+    .then(function(data){
+        console.log(data);
+        for( let i = 0; i < data.length; i++){
+
+
+
+
+    }
+})
+}
