b7a47c30c292686cea2fc87c3184fd2cb36fe47d
Pema Sherpa <pematamang111@gmail.com>
2024-03-15 20:08:27 -0400

    add fetch function

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 //dependencies=========================
 const weatherDetail = document.querySelector('#weatherDetail');
 const dayFive = document.querySelector('#dayFive');
+const searchButton = document.querySelector('#search');
 const apiKey = 7cb9b0e18cc07a883b919195c72f4dcd;
 
 
@@ -15,10 +16,26 @@ fetch(weatherUrl)
     .then(function(data){
         console.log(data);
         for( let i = 0; i < data.length; i++){
+            const today = document.createElement('h2');
+            const todayTemp = document.createElement('p');
+            const todayWind = document.createElement('p');
+            const todayHumidity = document.createElement('p');
 
+            const todayValue = today.data[i]
+            const todayTempValue = todayTemp.data[i]
+            const todayWindValue = todayWind.data[i]
+            const todayHumidityValue = todayHumidity.data[i]
+
+            weatherDetail.append(todayValue);
+            weatherDetail.append(todayTempValue);
+            weatherDetail.append(todayWindValue);
+            weatherDetail.append(todayHumidityValue);
 
 
 
     }
 })
 }
+
+//userinterreaction===========================
+searchButton.addEventListener('click', getApi);
